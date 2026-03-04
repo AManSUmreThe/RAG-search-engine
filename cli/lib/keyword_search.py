06202cb26e9c9ad970926ff216f42ad5d830914f
AManSUmreThe <amanumrethe@gmail.com>
2026-03-04 02:41:18 +0000

    implementing tokenization

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,16 +1,33 @@
 from lib.search_utils import load_movies_data
 import string
+
+# puncuation
 def puncuate(text):
     text = text.lower()
     text = text.translate(str.maketrans("","",string.punctuation))
     return text
 
-def search_movies(keyword,n_results = 5):
+# tokenization
+def tokenize(text):
+    text = puncuate(text)
+    tokens = [token for token in text.split() if token]
+    return tokens
+
+# Checking match between keyword and movie tokens
+def check_match(keywords,movie_tokens):
+    for keyword in keywords:
+        for movie_token in movie_tokens:
+            if keyword in movie_token:
+                return True
+    return False
+
+def search_movies(keywords,n_results = 5):
     movies = load_movies_data()
     results = []
-    keyword = puncuate(keyword)
+    keywords = tokenize(keywords)
     for movie in movies:
-        if keyword in puncuate(movie['title']):
+        movie_tokens = tokenize(movie['title'])
+        if check_match(keywords,movie_tokens):
             results.append(movie)
         if len(results) == n_results:
             break
