e559943c9d09c4754d2b9e8fc9ec9ba602dfca0f
AManSUmreThe <amanumrethe@gmail.com>
2026-03-04 02:26:58 +0000

    puncuation removal in searh

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -18,7 +18,7 @@ def main() -> None:
             # print the search query here
             print(f"Searching for: {args.query}")
             
-            results = search_movies(args.query)
+            results = search_movies(args.query,200)
             for i,movie in enumerate(results):
                 print(f"{i}. {movie['title']}")
         case _:
--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,10 +1,16 @@
 from lib.search_utils import load_movies_data
+import string
+def puncuate(text):
+    text = text.lower()
+    text = text.translate(str.maketrans("","",string.punctuation))
+    return text
 
 def search_movies(keyword,n_results = 5):
     movies = load_movies_data()
     results = []
+    keyword = puncuate(keyword)
     for movie in movies:
-        if keyword in movie['title']:
+        if keyword in puncuate(movie['title']):
             results.append(movie)
         if len(results) == n_results:
             break
