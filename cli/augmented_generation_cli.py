5e357685dfa23d2d9b650fb75ae8a8b4d8761faf
AManSUmreThe <amanumrethe@gmail.com>
2026-03-26 16:56:20 +0000

    Answer with citation

--- cli/augmented_generation_cli.py
+++ cli/augmented_generation_cli.py
@@ -1,6 +1,6 @@
 import argparse
 from lib.hybrid_search import rrf_search, weighted_search
-from lib.rag import answer_query, summarize
+from lib.rag import answer_query, summarize, citations
 
 def main():
     parser = argparse.ArgumentParser(description="Retrieval Augmented Generation CLI")
@@ -10,11 +10,14 @@ def main():
         "rag", help="Perform RAG (search + generate answer)"
     )
     rag_parser.add_argument("query", type=str, help="Search query for RAG")
-    summary_parser = subparsers.add_parser(
-        "summarize", help="Perform RAG (search + generate answer)"
-    )
+
+    summary_parser = subparsers.add_parser("summarize", help="Perform RAG (search + generate answer)")
     summary_parser.add_argument("query", type=str, help="Search query for RAG")
     summary_parser.add_argument("--limit", type=int, default=5, help="Search results limit")
+
+    citation_parser = subparsers.add_parser("citation", help="Perform RAG (search + generate answer + citations)")
+    citation_parser.add_argument("query", type=str, help="Search query for RAG")
+    citation_parser.add_argument("--limit", type=int, default=5, help="Search results limit")
 
 
     args = parser.parse_args()
@@ -38,11 +41,23 @@ def main():
             results = rrf_search(query,k=60,limit=args.limit,enhances=None,rerank=None)
 
             print('Search Results: ')
-            for idx,res in enumerate(results,start=1):
-                print(f'{idx}. {res['title']}')
+            for res in results:
+                print(f'     - {res['title']}')
             
             print('LLM Summary:')
             response = summarize(query,results)
+            print(response)
+        case 'citation':
+            query = args.query
+            # do RAG stuff here
+            results = rrf_search(query,k=60,limit=args.limit,enhances=None,rerank=None)
+
+            print('Search Results: ')
+            for res in results:
+                print(f'     - {res['title']}')
+            
+            print('LLM Answer:')
+            response = citations(query,results)
             print(response)
         case _:
             parser.print_help()
